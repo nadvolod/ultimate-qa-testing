0538474614c24a17407a2e025a9be02ef37c575a
nadvolod <nadvolod@gmail.com>
2020-07-23 17:40:46 -0400

    add screener runner test for sel java lander

---
+++ screener.config.js
@@ -0,0 +1,25 @@
+var Steps = require('screener-runner/src/steps');
+
+module.exports = {
+  // full repository name for your project:
+  projectRepo: 'Ultimate QA',
+
+  // this example assumes Environment Variables listed below exist on your system:
+  apiKey: process.env.SCREENER_API_KEY,
+
+  // array of UI states to capture visual snapshots of.
+  // each state consists of a url and a name.
+  states: [
+    {
+      //This page has dynamic elements that only appear when they are scrolled into view
+      //so we run a script to preload those elements
+      url: 'https://ultimateqa.com/selenium-webdriver-java-course',
+      name: 'Selenium Java landing page on desktop',
+      steps: new Steps()
+      .executeScript('window.scrollTo(0,document.body.scrollHeight)')
+      .wait(4000)
+      .snapshot('Loaded')
+      .end()
+    }
+  ]
+};
