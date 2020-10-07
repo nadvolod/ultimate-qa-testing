72ada295ebe1af12b03eadb937eab33f72ae2152
nadvolod <nadvolod@gmail.com>
2020-10-07 16:55:47 -0400

    add working visual tests

--- test/configs/wdio.visual.conf.js
+++ test/configs/wdio.visual.conf.js
@@ -1,22 +1,92 @@
-const {config} = require('./wdio.conf');
+const {config} = require('./wdio.shared.conf');
+const sauceOptions = {
+    username: process.env.SAUCE_USERNAME,
+    accesskey: process.env.SAUCE_ACCESS_KEY
+};
+const visualOptions = {
+    apiKey: process.env.SCREENER_API_KEY,
+    projectName: 'ultimate-qa-testing',
+    scrollAndStitchScreenshots: true
+};
 
 config.region = process.env.REGION || 'us';
 
+config.specs = [
+    './test/specs/**/*visual.spec.js'
+],
+
+//Screener Configuration
+config.hostname = 'hub.screener.io',
+config.port = 443,
+config.protocol = 'https',
+config.path = '/wd/hub'
+
 config.capabilities = [
+    //Desktop A 28%: https://www.w3schools.com/browsers/browsers_display.asp
     {
         browserName: 'chrome',
+        platformName: 'windows 10',
+        browserVersion: 'latest',
+        'sauce:options': {
+            ...sauceOptions,
+        },
+        'sauce:visual': {
+            ...visualOptions,
+            viewportSize: '1280x1024'
+        }
+    },
+    //Desktop B 20%: https://www.w3schools.com/browsers/browsers_display.asp
+    {
+        browserName: 'chrome',
+        platformName: 'windows 10',
+        browserVersion: 'latest',
+        'sauce:options': {
+            ...sauceOptions,
+        },
+        'sauce:visual': {
+            ...visualOptions,
+            viewportSize: '1920x1080'
+        }
+    },
+    //iphone X
+    {
+        browserName: 'safari',
         platformName: 'macOS 10.15',
         browserVersion: 'latest',
         'sauce:options': {
-            username: process.env.SAUCE_USERNAME,
-            accesskey: process.env.SAUCE_ACCESS_KEY
+            ...sauceOptions,
         },
         'sauce:visual': {
-            apiKey: process.env.SCREENER_API_KEY,
-            projectName: 'ultimate-qa-testing',
-            viewportSize: '1280x1024'
+            ...visualOptions,
+            viewportSize: '375x812'
         }
-    }
+    },
+    //iphone 6-8
+    {
+        browserName: 'safari',
+        platformName: 'macOS 10.15',
+        browserVersion: 'latest',
+        'sauce:options': {
+            ...sauceOptions,
+        },
+        'sauce:visual': {
+            ...visualOptions,
+            viewportSize: '414x736'
+        }
+    },
+    //Galaxy S8+
+    {
+        browserName: 'chrome',
+        platformName: 'windows 10',
+        browserVersion: 'latest',
+        'sauce:options': {
+            ...sauceOptions,
+        },
+        'sauce:visual': {
+            ...visualOptions,
+            viewportSize: '360x740'
+        }
+    },
 ];
 
 exports.config = config;
