b0b73d3a1325d291a6bed01700e688374e6a128f
nadvolod <nadvolod@gmail.com>
2020-10-12 13:00:45 -0400

    add perf and visual configs

--- test/specs/perf.spec.js
+++ test/specs/perf.spec.js
@@ -1,15 +1,17 @@
 describe('Performance Tests', () => {
-    it('(sauce:performance) custom command should assert pageload and speedIndex has not regressed', () => {
-        browser.url('/');
-        const output = browser.execute('sauce:performance', {
-            name: 'WebdriverIO Performance Example',
-            metrics: ['speedIndex', 'load'],
-        })
-        const { result, details } = output
-        return assert.equal(
-            result, 'pass',
-            `Regression detected for metrics ${JSON.stringify(details, null, 4)}`,
-        )
+    it('Pages have not regressed', () => {
+        browser.url('/?utm_source=automated-testing');
+        browser.url('selenium-java-2/?utm_source=automated-testing');
+        browser.url('selenium-webdriver-java-course/?utm_source=automated-testing');
+        // const output = browser.execute('sauce:performance', {
+        //     name: 'Home Page Performance',
+        //     metrics: ['speedIndex', 'load'],
+        // })
+        // const { result, details } = output
+        // return assert.equal(
+        //     result, 'pass',
+        //     `Regression detected for metrics ${JSON.stringify(details, null, 4)}`,
+        // )
     })
 });
 
