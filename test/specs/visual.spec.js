70783d4e18488a9c4787ce5a53f4fdd289910e41
nadvolod <nadvolod@gmail.com>
2020-10-12 14:14:04 -0400

    update tests

--- test/specs/visual.spec.js
+++ test/specs/visual.spec.js
@@ -5,13 +5,16 @@ describe('Visual testing of landing pages', () => {
         browser.execute('/*@visual.snapshot*/', 'Home Page');
 
         browser.url('/selenium-java-sale/?utm_source=automated-testing');
-        browser.execute('/*@visual.snapshot*/', 'Sales Page');
+        browser.execute('window.scrollTo(0,document.body.scrollHeight)');
+        browser.execute('/*@visual.snapshot*/', 'Sales Page', {scrollAndStitchScreenshot: true});
 
         browser.url('/selenium-webdriver-java-course/?utm_source=automated-testing');
-        browser.execute('/*@visual.snapshot*/', 'Selenium Java Landing Page');
+        browser.execute('window.scrollTo(0,document.body.scrollHeight)');
+        browser.execute('/*@visual.snapshot*/', 'Selenium Java Landing Page', {scrollAndStitchScreenshot: true});
 
         browser.url('/selenium-java-2/?utm_source=automated-testing');
-        browser.execute('/*@visual.snapshot*/', 'Selenium Java Landing Page v2');
+        browser.execute('window.scrollTo(0,document.body.scrollHeight)');
+        browser.execute('/*@visual.snapshot*/', 'Selenium Java Landing Page v2', {scrollAndStitchScreenshot: true});
         browser.execute('/*@visual.end*/');
     });
 });
