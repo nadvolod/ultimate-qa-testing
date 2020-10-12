6f024c46af0518b224a751746b0cd74d40d56bad
nadvolod <nadvolod@gmail.com>
2020-10-12 13:29:33 -0400

    cleanup the test names

--- test/specs/perf.spec.js
+++ test/specs/perf.spec.js
@@ -1,17 +1,12 @@
-describe('Performance Tests', () => {
-    it('Pages have not regressed', () => {
+describe('Performance test for', () => {
+    it('ultimateqa.com', () => {
         browser.url('/?utm_source=automated-testing');
+    })
+    it('/selenium-java-2', () => {
         browser.url('selenium-java-2/?utm_source=automated-testing');
-        browser.url('selenium-webdriver-java-course/?utm_source=automated-testing');
-        // const output = browser.execute('sauce:performance', {
-        //     name: 'Home Page Performance',
-        //     metrics: ['speedIndex', 'load'],
-        // })
-        // const { result, details } = output
-        // return assert.equal(
-        //     result, 'pass',
-        //     `Regression detected for metrics ${JSON.stringify(details, null, 4)}`,
-        // )
+    })
+    it('/selenium-webdriver-java-course', () => {
+        browser.url('/selenium-webdriver-java-course/?utm_source=automated-testing');
     })
 });
 
