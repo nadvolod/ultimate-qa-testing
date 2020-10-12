65c9a3c0b982335879e4615c3ec660c776f10e43
nadvolod <nadvolod@gmail.com>
2020-10-12 13:31:47 -0400

    remove other perf and visual tests from ci

--- test/specs/complete-java-lander-v2-visualb.spec.js
+++
@@ -1,7 +0,0 @@
-describe('Selenium Java course landing page', () => {
-    it('looks good responsive', () => {
-        browser.url('/selenium-java-2/');
-        browser.execute('/*@visual.init*/', 'Selenium Java v2');
-        browser.execute('/*@visual.snapshot*/', 'Landing Page');
-    });
-});
