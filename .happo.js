6bfa22cd820c95b57782571f12a4ad6d17b7efec
nadvolod <nadvolod@gmail.com>
2022-07-05 09:24:11 -0400

    add happo.js

---
+++ .happo.js
@@ -0,0 +1,52 @@
+const { RemoteBrowserTarget } = require("happo.io");
+
+module.exports = {
+  apiKey: process.env.HAPPO_API_KEY,
+  apiSecret: process.env.HAPPO_API_SECRET,
+  project: "uqa-testing",
+  targets: {
+    "chrome-1080p": new RemoteBrowserTarget("chrome", {
+      viewport: "1920x1080",
+      freezeAnimations: "last-frame",
+    }),
+    // all viewports https://viewportsizer.com/devices/
+    // 	Samsung Galaxy S10+, S10
+    "chrome-galaxy-s10": new RemoteBrowserTarget("chrome", {
+      viewport: "360x740",
+      freezeAnimations: "last-frame",
+    }),
+    // iphone viewports https://yesviz.com/iphones.php
+    "safari-1080p": new RemoteBrowserTarget("safari", {
+      viewport: "1920x1080",
+      freezeAnimations: "last-frame",
+    }),
+    // iphone 12 pro
+    "safari-iphone-12-pro": new RemoteBrowserTarget("safari", {
+      viewport: "390x844",
+      freezeAnimations: "last-frame",
+    }),
+    "Apple-iPad-Pro-12.9": new RemoteBrowserTarget("safari", {
+      viewport: "1024x1366",
+      freezeAnimations: "last-frame",
+    }),
+    "Apple-iPad-Air": new RemoteBrowserTarget("safari", {
+      viewport: "834x1112",
+      freezeAnimations: "last-frame",
+    }),
+    "MacBook Pro 13.3": new RemoteBrowserTarget("safari", {
+      viewport: "1280x800",
+      freezeAnimations: "last-frame",
+    }),
+    "MacBook Pro 16": new RemoteBrowserTarget("safari", {
+      viewport: "1536x960",
+      freezeAnimations: "last-frame",
+    }),
+    // iphone 12 pro
+    "chrome-650": new RemoteBrowserTarget("chrome", {
+      viewport: "650x415",
+      freezeAnimations: "last-frame",
+    }),
+    // only works on public urls
+    // pages: [{ url: 'http://localhost:3000/', title: 'Home Page' }],
+  },
+};
