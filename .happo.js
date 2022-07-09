330fd3710c5c08c4677cfd4737cf763c4b45736c
nadvolod <nadvolod@gmail.com>
2022-07-09 14:12:47 -0400

    stable happo screenshots

--- .happo.js
+++ .happo.js
@@ -19,27 +19,34 @@ module.exports = {
     "safari-1080p": new RemoteBrowserTarget("safari", {
       viewport: "1920x1080",
       freezeAnimations: "last-frame",
+      // so that safari won't cut off the screenshot at window height
+      scrollStitch: true,
     }),
     // iphone 12 pro
     "safari-iphone-12-pro": new RemoteBrowserTarget("safari", {
       viewport: "390x844",
       freezeAnimations: "last-frame",
+      scrollStitch: true,
     }),
     "Apple-iPad-Pro-12.9": new RemoteBrowserTarget("safari", {
       viewport: "1024x1366",
       freezeAnimations: "last-frame",
+      scrollStitch: true,
     }),
     "Apple-iPad-Air": new RemoteBrowserTarget("safari", {
       viewport: "834x1112",
       freezeAnimations: "last-frame",
+      scrollStitch: true,
     }),
     "MacBook Pro 13.3": new RemoteBrowserTarget("safari", {
       viewport: "1280x800",
       freezeAnimations: "last-frame",
+      scrollStitch: true,
     }),
     "MacBook Pro 16": new RemoteBrowserTarget("safari", {
       viewport: "1536x960",
       freezeAnimations: "last-frame",
+      scrollStitch: true,
     }),
     // iphone 12 pro
     "chrome-650": new RemoteBrowserTarget("chrome", {
